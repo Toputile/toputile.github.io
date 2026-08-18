d510bcf86ec6454dc73766ad2fdb258ec671a546
Toputile <axou944@gmail.com>
2026-08-18 18:43:43 +0200

    Update affiliate-links.js

--- assets/js/affiliate-links.js
+++ assets/js/affiliate-links.js
@@ -2,13 +2,13 @@
 // Laisse une valeur vide tant que tu n'as pas encore ton lien affilié : le site utilisera une recherche Amazon normale.
 window.AFFILIATE_LINKS = {
   'support-magsafe': 'https://link.amazon/B0cCQ9M9v',
-  'xibao-360': '',
-  'ohfamo-gonfleur': '',
-  'acezuk-booster': '',
-  'fontaine-chat': '',
-  'apauk-fontaine': '',
-  'wecook-casserole': '',
-  'soundcore-q20i': '',
-  'fire-tv-stick-hd': '',
-  'airtag-2': '',
+  'xibao-360': 'https://link.amazon/B0fRSS9M4',
+  'ohfamo-gonfleur': 'https://link.amazon/B0eZAZjec',
+  'acezuk-booster': 'https://link.amazon/B03pG6S6B',
+  'fontaine-chat': 'https://link.amazon/B0g7gs0BO',
+  'apauk-fontaine': 'https://link.amazon/B02ruKTzz',
+  'wecook-casserole': 'https://link.amazon/B03CiVzv0',
+  'soundcore-q20i': 'https://link.amazon/B05wryqFC',
+  'fire-tv-stick-hd': 'https://link.amazon/B056NY0pJ',
+  'airtag-2': 'https://link.amazon/B0cqdqkxe',
 };
