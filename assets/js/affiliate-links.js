af61227eeaa91ed8374adb05a7b217fd6be22c78
Toputile <axou944@gmail.com>
2026-08-19 08:09:25 +0200

    Update affiliate link for support-magsafe

--- assets/js/affiliate-links.js
+++ assets/js/affiliate-links.js
@@ -1,7 +1,7 @@
 // Colle ici les LIENS SPECIAUX générés depuis ton compte Amazon Partenaires / SiteStripe.
 // Laisse une valeur vide tant que tu n'as pas encore ton lien affilié : le site utilisera une recherche Amazon normale.
 window.AFFILIATE_LINKS = {
-  'support-magsafe': 'https://link.amazon/B0cCQ9M9v',
+  'support-magsafe': 'https://www.amazon.fr/dp/B0GHYCJVLF/ref=nosim?tag=toputile01-21',
   'xibao-360': 'https://link.amazon/B0fRSS9M4',
   'ohfamo-gonfleur': 'https://link.amazon/B0eZAZjec',
   'acezuk-booster': 'https://link.amazon/B03pG6S6B',
